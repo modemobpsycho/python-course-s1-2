c8ceaf07122570cb94fb112c3fb238b31f5a2bed
Vadim <hawaiianwork@mail.ru>
2023-11-21 15:43:00 +0300

    .

---
+++ bool-comparison/example.py
@@ -0,0 +1,21 @@
+print(type(False))
+print(type(True))
+
+a = 5
+b = 7
+c = 5
+
+print(a == b)
+print(a != b)
+print(a == c)
+
+print('-'*10)
+print(a > b)
+print(a < b)
+print(a > c)
+
+print('-'*10)
+print(a >= b)
+print(a <= b)
+print(a >= c)
+print(a <= c)
