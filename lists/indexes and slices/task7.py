886f17c03e2efa8e4d74b133d70032a5aa69fbfc
Vadim <hawaiianwork@mail.ru>
2023-11-27 23:20:05 +0300

    done

---
+++ lists/indexes and slices/task7.py
@@ -0,0 +1,5 @@
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+n = int(input())
+# Получаем сокращенное название месяца по индексу в списке и выведите его
+print(months[n-1])
