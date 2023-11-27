fb00ca2c008e13743521036904e096f7b56745f0
Vadim <hawaiianwork@mail.ru>
2023-11-27 23:21:46 +0300

    .

---
+++ lists/methods/task5.py
@@ -0,0 +1,7 @@
+numbers = [-214, 777, 181, 9, 32, -139, 43, 89, 77, 448, -20, -917, 54,
+           5, 432, 43, 32, 422, -895, 7, 198, 284, 472, 3, -986, -964, -989, 29]
+numbers.remove(3)
+numbers.remove(5)
+numbers.remove(7)
+numbers.remove(9)
+print(numbers)
