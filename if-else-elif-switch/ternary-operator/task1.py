f898e3bc8b7b8048af0f0f615aa07e7d6f232c6c
Vadim <hawaiianwork@mail.ru>
2023-11-27 23:30:00 +0300

    .

---
+++ if-else-elif-switch/ternary-operator/task1.py
@@ -0,0 +1,3 @@
+number = int(input('Введите число: '))
+abs_number = number if number >= 0 else -number
+print(abs_number)
---
+++ if-else-elif-switch/ternary-operator/task2.py
@@ -0,0 +1,3 @@
+n = int(input())
+text = 'Even' if n % 2 == 0 else 'Odd'
+print(text)
