132273bfd8b23ea660246c1c97606b3a7126d23b
Vadim <hawaiianwork@mail.ru>
2023-12-06 23:51:57 +0300

    updates

---
+++ 6. dict-none-set-tuples/3. dict/task1.py
@@ -0,0 +1,13 @@
+sweet = {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "ppu": 0.55,
+    "calories": 125,
+}
+sweet['weight'] = 230       
+sweet['have_topping'] = True  
+
+sweet['name'] = 'SuperCake'
+sweet['calories'] = 350
+print(sweet)
---
+++ 6. dict-none-set-tuples/3. dict/task3.py
@@ -0,0 +1,6 @@
+num = int(input())  
+numbers = {}      
+
+for i in range(1, num+1):
+    numbers[i] = i ** 2
+print(numbers)
